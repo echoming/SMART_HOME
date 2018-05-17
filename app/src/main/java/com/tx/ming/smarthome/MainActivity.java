6559060179b15f7741b8c830bcad400632c7ea7d
echoming <38649332+echoming@users.noreply.github.com>
2018-05-17 23:20:46 +0800

    Initial commit

---
+++ app/src/main/java/com/tx/ming/smarthome/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tx.ming.smarthome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
